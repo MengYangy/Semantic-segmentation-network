b01bd8f443eaa2a850b6a8d6f16f579204c6d194
MengYangy <mengyy1029@gmail.com>
2021-06-10 16:20:34 +0800

    跟新

--- utili/GPUSet.py
+++ utili/GPUSet.py
@@ -8,6 +8,23 @@ def setGpu():
     :return:
     '''
     gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+    # cpus = tf.config.experimental.list_physical_devices(device_type='CPU')
+
+    # # 设置GPU使用显存为4GB
+    # tf.config.experimental.set_virtual_device_configuration(
+    #     gpus[0],
+    #     [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)]
+    # )
+
+    # 单个物理GPU模拟多个GPUS
+    # tf.config.experimental.set_virtual_device_configuration(
+    #     gpus[0],
+    #     [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048),
+    #      tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)])
+
+    # 指定训练GPU
+    # import os
+    # os.environ['CUDA_VISIBLE_DEVICES']='2,3'
     print('GPU:', gpus)
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
